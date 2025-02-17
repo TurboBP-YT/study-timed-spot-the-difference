69b45212c3b8173617781b2c9a84c034f84dca35
TurboBP-YT <turboblackpill@protonmail.com>
2025-02-16 23:06:11 -0600

    .

--- api/db.ts
+++ api/db.ts
@@ -2,6 +2,7 @@ import { MongoClient, ServerApiVersion } from 'mongodb';
 const uri:string = "mongodb+srv://<db_username>:<db_password>@cluster0.4ruts.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 export function getClient():MongoClient {
+  console.log('db username = ' + process.env['db_username'])
     return new MongoClient(uri.replace('<db_username>',process.env['db_username']).replace('<db_password>',process.env['db_password']), {
         serverApi: {
         version: ServerApiVersion.v1,
